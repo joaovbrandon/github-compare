92c558dbf2269ffd4c3f5e00024a72252a49a2d6
joaaoeu <hello@joaao.eu>
2019-03-23 15:09:46 -0300

    Updates

---
+++ precache-manifest.3c860910afd09a0b8ca7cba1ac316404.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/github-compare/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/github-compare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/github-compare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/github-compare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/github-compare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "638eb0bf8b12a538e83a",
+    "url": "/github-compare/static/js/runtime~main.d4e076fe.js"
+  },
+  {
+    "revision": "b4f441be4abc1b15c0b2",
+    "url": "/github-compare/static/js/main.145aede9.chunk.js"
+  },
+  {
+    "revision": "b1d32372293ddd1378bd",
+    "url": "/github-compare/static/js/2.0063404b.chunk.js"
+  },
+  {
+    "revision": "b1d32372293ddd1378bd",
+    "url": "/github-compare/static/css/2.2078e980.chunk.css"
+  },
+  {
+    "revision": "84da6fe2bce2b1e3e131087aabf7d108",
+    "url": "/github-compare/index.html"
+  }
+];
